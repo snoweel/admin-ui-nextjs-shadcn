d60c4774c1ba31eb003a76d7a8c372c3ecacfc70
Snowel Pereira <snoweel.pererira@google.com>
2024-06-29 17:10:06 -0230

    - post edit page with back button

---
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -0,0 +1,12 @@
+import BackButton from "@/components/BackButton";
+
+const PostEditpage = () => {
+  return (
+    <>
+      <BackButton text="Back to Posts" link="/posts" />
+      <h1>Edit</h1>
+    </>
+  );
+};
+
+export default PostEditpage;

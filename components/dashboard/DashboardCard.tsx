07e1ad578635d68259cb0b110845d89f429578ce
Snowel Pereira <snoweel.pererira@google.com>
2024-06-29 14:52:50 -0230

    wip

---
+++ components/dashboard/DashboardCard.tsx
@@ -0,0 +1,30 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Newspaper, LucideIcon } from "lucide-react";
+import React from "react";
+
+interface DashboardProps {
+  title: string;
+  count: number;
+  icon: React.ReactElement<LucideIcon>;
+}
+
+const DashboardCard = ({ title, count, icon }: DashboardProps) => {
+  return (
+    <Card className="bg-slate-100 dark:bg-slate-800 p-4 pb-0">
+      <CardContent>
+        <h3 className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">
+          {title}
+        </h3>
+        <div className="flex gap-5 justify-center items-center">
+          {icon}
+          {/* <Newspaper className="text-slate-500" size={72} /> */}
+          <h3 className="text-5xl font-semibold text-slate-500 dark:text-slate-200">
+            {count}
+          </h3>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default DashboardCard;

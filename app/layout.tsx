e3fd6b50d43796bea41659ccb38a96f519b5c21b
Snowel Pereira <snoweel.pererira@google.com>
2024-06-29 19:22:24 -0230

    reafstuct layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
+
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
@@ -28,13 +27,7 @@ export default function RootLayout({
           storageKey="dashboard-theme"
           // disableTransitionOnChange
         >
-          <Navbar />
-          <div className="flex">
-            <div className="hidden md:block h-[100vh] w-[300px]">
-              <Sidebar />
-            </div>
-            <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
-          </div>
+          {children}
           <Toaster />
         </ThemeProvider>
       </body>

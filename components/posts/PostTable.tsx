5047fa3f6f039babd8e5302aece95e8150928262
Snowel Pereira <snoweel.pererira@google.com>
2024-06-29 15:52:26 -0230

    basic post UI

---
+++ components/posts/PostTable.tsx
@@ -0,0 +1,58 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCaption,
+} from "@/components/ui/table";
+import Link from "next/link";
+
+import posts from "@/data/posts";
+import { Post } from "@/types/posts";
+
+interface PostsTableProps {
+  limit?: number;
+  title?: string;
+}
+const PostTable = ({ title, limit }: PostsTableProps) => {
+  return (
+    <div className="mt-10">
+      <h3 className="text-2xl mb-4 font-semibold">{title ? title : "Posts"}</h3>
+      <Table>
+        <TableCaption>List of Recent Posts</TableCaption>
+        <TableHeader>
+          <TableHead> Title</TableHead>
+          <TableHead className="hidden md:table-cell"> Author</TableHead>
+          <TableHead className="hidden md:table-cell text-right">
+            Date
+          </TableHead>
+          <TableHead> View</TableHead>
+        </TableHeader>
+        <TableBody>
+          {posts.map((post) => (
+            <TableRow key={post.id}>
+              <TableCell>{post.title}</TableCell>
+              <TableCell className="hidden md:table-cell">
+                {post.author}
+              </TableCell>
+              <TableCell className="hidden md:table-cell text-right">
+                {post.date}
+              </TableCell>
+              <TableCell>
+                <Link href={`/posts/edit/${post.id}`}>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs">
+                    Edit
+                  </button>
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default PostTable;

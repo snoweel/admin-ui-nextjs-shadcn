27942f6f983bc3420b7444a458ad81ce4343e65c
Snowel Pereira <snoweel.pererira@google.com>
2024-06-29 20:29:21 -0230

    theme toggle on login page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,13 @@
+import ThemeToggler from "@/components/ThemeToggler";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-[100vh] flex items-center justify-center relative">
+      <div className="absolute bottom-5 right-0 text-white">
+        <ThemeToggler />
+      </div>
       {children}
     </div>
   );
